fad8f4cddf218e0f987e72b29c89e1cd73f7e0de
microprediction <info@3za.org>
2020-05-14 12:16:19 -0400

    README / version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="pandemic",
-    version="0.3.13",
+    version="0.4.0",
     description="Orstein-Uhlenbeck pandemic simulation",
     long_description=README,
     long_description_content_type="text/markdown",
